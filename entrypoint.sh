d2aa43f60dcf49f447f214c916079d918d51fb76
haya14busa <haya14busa@gmail.com>
2020-07-06 15:03:32 +0000

    pipe stderr

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-alex ${INPUT_ALEX_FLAGS} . \
+alex ${INPUT_ALEX_FLAGS} . 2>&1 >/dev/null \
   | reviewdog \
       -efm='%-P%f' \
       -efm=' %#%l:%c-%[0-9]%#:%[0-9]%# %# %trror  %m' \
